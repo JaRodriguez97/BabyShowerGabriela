f0c31669f3ed0447484ca74751347016a1d37fae
Jesus rodriguez <cristi542012@gmail.com>
2025-07-05 18:40:53 -0500

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'splash', pathMatch: 'full' },
+  {
+    path: 'splash',
+    loadChildren: () =>
+      import('./modules/splash/splash.module').then((m) => m.SplashModule),
+  },
+  {
+    path: 'inicio',
+    loadChildren: () =>
+      import('./modules/inicio/inicio.module').then((m) => m.InicioModule),
+    title: 'Bienvenido al Baby Shower de la Pequeña Daniela',
+  },
+  // {
+  //   path: 'marco',
+  //   loadChildren: () =>
+  //     import('./modules/marco/marco.module').then((m) => m.MarcoModule),
+  //   title: 'Protagonista del Baby Shower',
+  // },
+  // {
+  //   path: 'confirmacion',
+  //   loadChildren: () =>
+  //     import('./modules/confirmacion/confirmacion.module').then(
+  //       (m) => m.ConfirmacionModule
+  //     ),
+  //   title: 'Confirma tu Asistencia al Mejor Baby Shower',
+  // },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabledBlocking'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/modules/inicio/inicio.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PublicService } from '@services/public/public.service';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css'],
+})
+export class InicioComponent implements OnInit {
+  constructor(public route: Router, public publicService: PublicService) {}
+
+  ngOnInit(): void {
+    if (!this.publicService.isPlaying()) this.publicService.playAudio();
+
+    // setTimeout(() => this.route.navigate(['marco']), 8000);
+  }
+}
